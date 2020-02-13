1bf710fb18a6a3d47ad4da15375d0c4786fe8e22
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-02-13 17:22:26 +0800

    Notify if pokemon does not exist

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -5,6 +5,7 @@ import os
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from pokemon_entities.models import Pokemon, PokemonEntity
+from django.core.exceptions import ObjectDoesNotExist
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832&fill=transparent"
